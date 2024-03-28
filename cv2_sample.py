e3cc6e23e5558bf59278707ed06950d9e0f2cc78
pascal65536 <pascal65536@gmail.com>
2024-03-28 18:16:59 +0700

    469355

--- cv2_sample.py
+++ cv2_sample.py
@@ -11,26 +11,26 @@ def smooth(I):
     return J
 
 
-num = '4'
+num = '3'
 
-foto = Image.open(f'img/{num}.png')
+foto = Image.open(f'data/image_{num}.png')
 new_size = (28, 28)
 foto.thumbnail(new_size)
-foto.save(f'img/{num}_28.png')
+foto.save(f'data/image_{num}_28.png')
 
 # read image as grey scale
-grey_img = cv2.imread(f"img/{num}_28.png", cv2.IMREAD_GRAYSCALE)
+grey_img = cv2.imread(f"data/image_{num}_28.png", cv2.IMREAD_GRAYSCALE)
 
 # save image
-status = cv2.imwrite(f"img/{num}_28_inp.png", grey_img)
+status = cv2.imwrite(f"data/image_{num}_28_inp.png", grey_img)
 print("Image written to file-system : ", status)
-np.savetxt(f"img/{num}_28_inp.csv", grey_img, delimiter=",", fmt='%d')
+np.savetxt(f"data/image_{num}_28_inp.csv", grey_img, delimiter=",", fmt='%d')
 
 denoise = smooth(grey_img)
 
 # save image
-status = cv2.imwrite(f"img/{num}_28_out.png", denoise)
+status = cv2.imwrite(f"data/image_{num}_28_out.png", denoise)
 print("Image written to file-system : ", status)
-np.savetxt(f"img/{num}_28_out.csv", denoise, delimiter=",", fmt='%d')
+np.savetxt(f"data/image_{num}_28_out.csv", denoise, delimiter=",", fmt='%d')
 
 # np.savetxt('test1.txt', grey_img, fmt='%d')
